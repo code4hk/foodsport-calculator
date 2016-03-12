14b493083b499b32df68b755d443299e3e146b1c
vincentlaucy <lauchunyin@gmail.com>
2016-03-12 21:20:02 +0800

    layout

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+require('normalize.css')
+require('lib/semantic/semantic.css')
+require('lib/smoothscroll.js')
+
+
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('example')
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path');
 module.exports = {
-    entry: './src/app/index.jsx',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: '[name].js',
-      chunkFilename: '[chunkhash].js',
-      libraryTarget: 'umd'
-    },
-    module: {
-        loaders: [
-            {
-                //tell webpack to use jsx-loader for all *.jsx files
-                test: /\.jsx$/,
-                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
-            },
-            {test: /\.js$/, loader: 'babel'}
-        ]
-    },
-    externals: {
-        //don't bundle the 'react' npm package with our bundle.js
-        //but get it from a global 'React' variable
-        'react': 'React'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-      plugins: [new HtmlWebpackPlugin({
-        // title: 'Custom template',
-        template: 'index.ejs', // Load a custom template
-        // inject: 'body' // Inject all scripts into the body
-      })]
-    //for DEV only TODO another config for PRD
-    // devtool:"eval"
+  entry: './src/app/index.jsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    chunkFilename: '[chunkhash].js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        //tell webpack to use jsx-loader for all *.jsx files
+        test: /\.jsx$/,
+        loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+        { test: /\.(woff(2)?|eot|svg|ttf)$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" },
+      {test: /\.js$/, loader: 'babel'}
+    ]
+  },
+  externals: {
+    //don't bundle the 'react' npm package with our bundle.js
+    //but get it from a global 'React' variable
+    'react': 'React'
+  },
+  resolve: {
+    root: [__dirname],
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [new HtmlWebpackPlugin({
+    // title: 'Custom template',
+    template: 'index.ejs', // Load a custom template
+    // inject: 'body' // Inject all scripts into the body
+  })]
+  //for DEV only TODO another config for PRD
+  // devtool:"eval"
 }
